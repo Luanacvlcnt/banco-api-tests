d391e2b4c8c250c27e1ad5fdc65791caf226a3c2
Luanacvlcnt <lua.cavalcanti19@gmail.com>
2026-05-13 17:03:10 -0300

    Adiciona testes para o GET transferencias total e individual

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -10,40 +10,73 @@ describe('Transferencias', () => {
     beforeEach(async () => {
         token = await obterToken('julio.lima', '123456')
     })
+
     describe('POST /transferencias', () => {
 
+        it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de 10,00', async () => {
+            const resposta = await request(process.env.BASE_URL)
+                .post('/transferencias')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', 'Bearer ' + token)
+                .send({
+                    contaOrigem: 1,
+                    contaDestino: 2,
+                    valor: 11,
+                    token: ""
+                })
+
+            expect(resposta.statusCode).to.equal(201);
+
+            console.log(resposta.body)
+        })
+
+        it('Deve retornar falha com 422 quando o valor da transferencia for abaixo de 10,00', async () => {
+            const bodyTranferencias = { ...postTransferencias }
+            bodyTranferencias.valor = 7
+
+            const resposta = await request(process.env.BASE_URL)
+                .post('/transferencias')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', 'Bearer ' + token)
+                .send(bodyTranferencias)
+
+            expect(resposta.statusCode).to.equal(422);
+
+            console.log(resposta.body)
+        })
     })
 
+    describe('GET /transferencias/{id}', () => {
 
-    it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de 10,00', async () => {
-        const resposta = await request(process.env.BASE_URL)
-            .post('/transferencias')
-            .set('Content-Type', 'application/json')
-            .set('Authorization', 'Bearer ' + token)
-            .send({
-                contaOrigem: 1,
-                contaDestino: 2,
-                valor: 11,
-                token: ""
-            })
+        it('Deve retornar sucesso com 200 e dados iguais ao registro de transferência contido no banco de dados quando o ID for válido', async () => {
+            const resposta = await request(process.env.BASE_URL)
+                .get('/transferencias/10')
+                .set('Authorization', 'Bearer ' + token)
 
-        expect(resposta.statusCode).to.equal(201);
+            console.log(resposta.status)
+            console.log(resposta.body)
+            expect(resposta.status).to.equal(200)
+            expect(resposta.body.id).to.equal(10)
+            expect(resposta.body.id).to.be.a('number')
+            expect(resposta.body.conta_origem_id).to.equal(1)
+            expect(resposta.body.conta_destino_id).to.equal(2)
+            expect(resposta.body.valor).to.equal(11.00)
+        })
 
-        console.log(resposta.body)
     })
 
-    it('Deve retornar falha com 422 quando o valor da transferencia for abaixo de 10,00', async () => {
-        const bodyTranferencias = { ...postTransferencias }
-        bodyTranferencias.valor = 7
+    describe('GET /transferencias', () => {
+        it('Deve retornar 10 elementos na paginação quando informar limite de 10 registros', async () => {
+            const resposta = await request(process.env.BASE_URL)
+                .get('/transferencias?page=1&limit=10')
+                .set('Authorization', 'Bearer ' + token)
 
-        const resposta = await request('http://localhost:3000')
-            .post('/transferencias')
-            .set('Content-Type', 'application/json')
-            .set('Authorization', 'Bearer ' + token)
-            .send(bodyTranferencias)
+            expect(resposta.status).to.equal(200)
+            expect(resposta.body.limit).to.equal(10)
+            expect(resposta.body.transferencias).to.have.lengthOf(10)
 
-        expect(resposta.statusCode).to.equal(422);
+        })
 
-        console.log(resposta.body)
     })
+
 })
